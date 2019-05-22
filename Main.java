fe1783267a423e56c6403b9f5f42c19ad3b220f3
Alex Brown <alexbrown@comcast.net>
2019-05-22 18:12:43 -0400

    Alice and Bob
    Code greet Alice and Bob by name.  Asks strangers who they are.

--- Main.java
+++ Main.java
@@ -1,9 +1,35 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args ){
-
+       
+        Scanner sc = new Scanner(System.in);
+        String nameIn = "" ;
+        
+        System.out.print("What is your name? ");
+        nameIn = sc.next();
+        
+        if (nameIn.equals("Alice") || nameIn.equals("Bob")) {
+            greeting(nameIn);
+        }
+        else {
+            who(nameIn);
+        }
+        return;
     }
+    
+    public static void greeting(String friend) {
+        System.out.println("Hello " + friend + ", Good to see you!");
+        return;
+    }
+    public static void who(String stranger) { 
+        System.out.println("Who are you " + stranger + "?");
+        return;   
+    }
+    
 }
